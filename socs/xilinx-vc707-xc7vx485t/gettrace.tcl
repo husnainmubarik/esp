3acad1837ab232992d72fdd032e68e58e7934368
Gabriele Tombesi <gabriele@don.cs.columbia.edu>
2020-09-09 18:24:35 -0400

    Synchronized tms

--- socs/xilinx-vc707-xc7vx485t/gettrace.tcl
+++ socs/xilinx-vc707-xc7vx485t/gettrace.tcl
@@ -1,31 +1,38 @@
 add list \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc1_input_port \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc1_data_void_in \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc1_stop_out \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc2_output_port \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc2_data_void_out \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc2_stop_in \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc3_input_port \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc3_data_void_in \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc3_stop_out \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc3_output_port \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc3_data_void_out \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc3_stop_in \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc4_input_port \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc4_data_void_in \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc4_stop_out \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc4_output_port \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc4_data_void_out \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc4_stop_in \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc5_input_port \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc5_data_void_in \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc5_stop_out \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc5_output_port \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc5_data_void_out \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc5_stop_in \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc6_input_port \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc6_data_void_in \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc6_stop_out \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc6_output_port \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc6_data_void_out \
-sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/sync_noc_set_cpu/noc6_stop_in 
+sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/noc1_input_port \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/noc1_cpu_data_void_in \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/noc1_cpu_stop_out \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/test2_output_port \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/test2_cpu_data_void_out \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/test2_cpu_stop_out \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/noc3_input_port \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/noc3_cpu_data_void_in \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/noc3_cpu_stop_out \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/test3_output_port \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/test3_cpu_data_void_out \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/test3_cpu_stop_out \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/noc4_input_port \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/noc4_cpu_data_void_in \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/noc4_cpu_stop_out \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/test4_output_port \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/test4_cpu_data_void_out \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/test4_cpu_stop_out \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/noc5_input_port \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/noc5_cpu_data_void_in \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/noc5_cpu_stop_out \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/test5_output_port \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/test5_cpu_data_void_out \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/test5_cpu_stop_out \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/noc6_input_port \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/noc6_cpu_data_void_in \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/noc6_cpu_stop_out \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/test6_output_port \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/test6_cpu_data_void_out \
+    sim:/testbench/top_1/esp_1/tiles_gen(1)/cpu_tile/tile_cpu_i/jtag_test_1/test6_cpu_stop_out
+configure list -usestrobe 0
+configure list -strobestart {0 ps} -strobeperiod {0 ps}
+configure list -usesignaltrigger 1
+configure list -delta none
+configure list -signalnamewidth 1
+configure list -datasetprefix -1
+configure list -namelimit 0
