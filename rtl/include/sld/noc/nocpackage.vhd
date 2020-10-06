a810a50307b7d18de53f70f81c6e3941f04690bf
Paolo Mantovani <paolo@cs.columbia.edu>
2020-10-06 17:36:11 -0400

    add FIFO variation with almost_full output

--- rtl/include/sld/noc/nocpackage.vhd
+++ rtl/include/sld/noc/nocpackage.vhd
@@ -171,6 +171,22 @@ package nocpackage is
       data_out    : out std_logic_vector(width-1 downto 0));
   end component;
 
+  component fifo3
+    generic (
+      depth : integer;
+      width : integer);
+    port (
+      clk         : in  std_logic;
+      rst         : in  std_logic;
+      rdreq       : in  std_logic;
+      wrreq       : in  std_logic;
+      data_in     : in  std_logic_vector(width-1 downto 0);
+      empty       : out std_logic;
+      full        : out std_logic;
+      almost_full : out std_logic;
+      data_out    : out std_logic_vector(width-1 downto 0));
+  end component;
+
   component inferred_async_fifo
     generic (
       g_data_width : natural := NOC_FLIT_SIZE;
